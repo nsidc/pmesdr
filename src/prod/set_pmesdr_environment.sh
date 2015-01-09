b87e600833b9bcebd2cc14c2cc9fc31bcd46955f
M. J. Brodzik <brodzik@nsidc.org>
2015-01-09 09:46:12 -0700

    Cleaned up set environment script, and added LOCALE env variable setting to its work.
    Former-commit-id: b113d8e66a21dec3db540ed7665288f47093cbe0

--- src/prod/set_pmesdr_environment.sh
+++ src/prod/set_pmesdr_environment.sh
@@ -1,14 +1,33 @@
 #!/usr/bin/env sh
 # set_pmesdr_environment.sh
 #
+# usage: set_pmesdr_environment.sh or set_pmesdr_environment.sh -c gcc
+#   Sets environment up for gcc compiler
+# usage: set_pmesdr_environment.sh -c icc
+#   Sets environment up for icc (Intel supercomputer) compiler
+#
 #  This script is used by sourcing it ". /this/script/location/set_pmesdr_environment.sh"
 #  By doing this, you set up the environment necessary for building and running the
 #  Passive Microwave ESDR system software.
+#  When this script is finished, the bash environment variables 
+#  that are used by our Makefiles and system processing will be set.
 #
 # 2014-04-15 M. J. Brodzik 303-492-8263 brodzik@nsidc.org
 # National Snow & Ice Data Center, University of Colorado, Boulder
 # Copyright (C) 2014 Regents of University of Colorado and Brigham-Young University
 #========================================================================
+
+# Parse command line
+compiler=gcc
+set -- $(getopt c: "$@")
+while [ $# -gt 0 ]
+do
+    case "$1" in
+    (-c) compiler="$2";;
+    (*) break;;
+    esac
+    shift
+done
 
 # Grab the full path to this script, regardless of where it is called from.
 export PMESDR_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -22,21 +41,33 @@ export SIR_areas=$PMESDR_TOP_DIR/ref/locs
 export SIR_region=$PMESDR_TOP_DIR/ref/regiondef1.dat
 export RSS_path=$PMESDR_TOP_DIR/ref
 
-#
-# test to see if this is being run on Janus, in which case you have modules to load before building
-# Also test to see which compiler on Janus is to be used
-#
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-  module load slurm
-  if [[ "$COMPILER" == "gcc" ]]; then
-    echo "gcc compiler"
-    module unload netcdf/netcdf4-4.3_hdf5-1.8.11_hdf4-4.2.9_szip-2.1_zlib-1.2.78_jpeglib-8d_intel-13.0.0
-    module load netcdf/netcdf4-4.3.2_hdf5-1.8.13_hdf4-4.2.10_szip-2.1_zlib-1.2.8_jpeglib-9a_openmpi-1.8.2_gcc-4.9.1
-  fi
-  if [[ "$COMPILER" == "icc" ]]; then
-    echo "icc compiler"
-    module load netcdf/netcdf4-4.3_hdf5-1.8.11_hdf4-4.2.9_szip-2.1_zlib-1.2.78_jpeglib-8d_intel-13.0.0
-    module unload netcdf/netcdf4-4.3.2_hdf5-1.8.13_hdf4-4.2.10_szip-2.1_zlib-1.2.8_jpeglib-9a_openmpi-1.8.2_gcc-4.9.1
-  fi   
-fi
+# netCDF libraries are different, depending on compiler
+icc_netcdf=netcdf/netcdf4-4.3_hdf5-1.8.11_hdf4-4.2.9_szip-2.1_zlib-1.2.78_jpeglib-8d_intel-13.0.0
+gcc_netcdf=netcdf/netcdf4-4.3.2_hdf5-1.8.13_hdf4-4.2.10_szip-2.1_zlib-1.2.8_jpeglib-9a_openmpi-1.8.2_gcc-4.9.1
 
+# Determine the LOCALE, a function of host and compiler.
+# Janus needs to load compiler-specific modules before building
+if [[ "$HOSTNAME" == *[Jj]"anus"* ]]; then
+  module load slurm
+  if [[ "$compiler" == "gcc" ]]; then
+    echo "Setting netcdf for the gcc compiler"
+    module unload $icc_netcdf
+    module load $gcc_netcdf
+    export LOCALE=JANUSgcc
+  fi
+  if [[ "$compiler" == "icc" ]]; then
+    echo "Setting netcdf for the icc compiler"
+    module unload $gcc_netcdf
+    module load $icc_netcdf
+    export LOCALE=JANUSicc
+  fi
+  module list
+elif [[ "$HOSTNAME" == "snow"* ]]; then
+    export LOCALE=NSIDCsnow
+elif [[ "$HOSTNAME" == *"dev"* ]]; then
+    export LOCALE=NSIDCdev
+else
+    export LOCALE=BYU
+fi # endif janus
+
+echo "PMESDR system LOCALE=$LOCALE, ready to make the system."
