b9f1319e1679d8bfbfd7ec43f126cb8f12f9ff86
Molly Hardman <mhardman@nsidc.org>
2015-01-09 18:11:57 -0700

    another change to run_setup_test.sh
    Former-commit-id: 6e7dedf22b7a51768d6f1b1dff662f705ab82e02

--- janus_batchfiles/run_setup_test.sh
+++ janus_batchfiles/run_setup_test.sh
@@ -1,41 +1,53 @@
 #!/bin/bash
+#========================================================================
+# janus batch file to create a setup file suitable for testing
+#
+# output of this batch file is the set of setup files created by the Makefile targets
+#     csu_ease and rss_ease
+#
+# 2015-01-09 M. A. Hardman 303-492-2969 mhardman@nsidc.org
+# National Snow & Ice Data Center, University of Colorado, Boulder
+# Copyright (C) 2015 Regents of University of Colorado and Brigham-Young University
+#========================================================================
+
 # Lines starting with #PBS are treated by bash as comments, but interpreted by qsub
 # as arguments.  
 
 #
 # Set the name of the job
-#SBATCH -J meta_setup
+base=${0##*/}
+base=${base%.*}
+#SBATCH -J ${base}
 
 #
 # Set a walltime for the job. The time format is HH:MM:SS 
-
-#SBATCH --time=00:59:59
-
-#
-# Select one node
-# SBATCH -N1
-
-# Select 1 task per node
-# SBATCH --ntasks-per-node 1
+#SBATCH --time=00:15:00
 
 # Set the output file and embed the job number in the filename
-#SBATCH -o output/meta_setup-%j.out
+#SBATCH -o ${PMESDR_TOP_DIR}/janus_batchfiles/output/${base}-%j.out
 
 # Select the janus QOS 
 #SBATCH --qos=janus
 
 # The following commands will be executed when this script is run.
 
-cd /projects/moha2290/PMESDR/mahworking/measures/src/prod
-source set_pmesdr_environment.sh
-# module load  netcdf/netcdf4-4.3_hdf5-1.8.11_hdf4-4.2.9_szip-2.1_zlib-1.2.78_jpeglib-8d_intel-13.0.0
+cd ${PMESDR_TOP_DIR}/src/prod
 
-cd meas_meta_setup/meas_meta_setup_CSU
+cd meas_meta_make
 date
-make LOCALE=JANUSicc csu_ease
-cd ../meas_meta_setup_RSS
+make rss_ease
 date
-make LOCALE=JANUSicc rss_ease
+make csu_ease
 date
-# End of example job shell script
+
+cd ../meas_meta_setup/meas_meta_setup_RSS
+date
+make rss_ease
+date
+
+cd ../meas_meta_setup_CSU
+make csu_ease
+date
+
+# End of script
 # 
