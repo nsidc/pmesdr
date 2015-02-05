f49a61f47831b24f692bcd0ab9c7533a71953024
M. J. Brodzik <brodzik@nsidc.org>
2015-02-05 10:03:34 -0700

    Merged regression test environment variable with python venv activates.
    Former-commit-id: 3e2d32e616c7ccbfd06b0c5accdc273e3553e484

--- src/prod/set_pmesdr_environment.sh
+++ src/prod/set_pmesdr_environment.sh
@@ -50,6 +50,7 @@ gcc_netcdf=netcdf/netcdf4-4.3.2_hdf5-1.8.13_hdf4-4.2.10_szip-2.1_zlib-1.2.8_jpeg
 # Janus needs to load compiler-specific modules before building
 if [[ "$HOSTNAME" == *[Jj]"anus"* || "$HOSTNAME" == *"rc.colorado.edu" ]]; then
   module load slurm
+  module load python/anaconda-2.1.0
   if [[ "$compiler" == "gcc" ]]; then
     echo "Setting netcdf for the gcc compiler"
     module unload $icc_netcdf
@@ -63,10 +64,15 @@ if [[ "$HOSTNAME" == *[Jj]"anus"* || "$HOSTNAME" == *"rc.colorado.edu" ]]; then
     export LOCALE=JANUSicc
   fi
   module list
+  export PATH=~/.conda/envs/pmesdr/bin:$PATH
 elif [[ "$HOSTNAME" == "snow"* ]]; then
     export LOCALE=NSIDCsnow
-elif [[ "$HOSTNAME" == *"dev"* ]]; then
+    # Initialize the virtualenv that was built for running on snow
+    . ~brodzik/.virtual_envs_snow/pmesdr/bin/activate
+elif [[ "$HOSTNAME" == "brodzik" ]]; then
     export LOCALE=NSIDCdev
+    # Initialize the virtualenv that was built for running on snow
+    . ~brodzik/.virtual_envs/pmesdr/bin/activate
 else
     export LOCALE=BYU
 fi # endif janus
