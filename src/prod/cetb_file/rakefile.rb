a3a3b641e74805d8262804b0f4a5bcd308aebc5a
M. J. Brodzik <brodzik@nsidc.org>
2015-07-10 16:07:18 -0600

    make/rake for cetb_file.
    Wrote a makefile to build the linkable library.
    Added cetb_file stuff to PMESDR project ueber-makefile.
    Added rake target to ceedling rakefile, to install the library when needed.
    
    
    Former-commit-id: 3ce47974562ad4b204469a12a37514198b2b2a84

--- src/prod/cetb_file/rakefile.rb
+++ src/prod/cetb_file/rakefile.rb
@@ -5,28 +5,14 @@ Ceedling.load_project
 
 task :default => %w[ test:all release ]
 
-desc "Install headers and linkable objects to PMESDR project."
+desc "Build and install headers and linkable objects to PMESDR project."
 task :install do
 
-  # Need to figure out how to make the include and lib dirs if
-  # they don't already exist
-  puts "Installing .o files:"
-  from = File.join(PROJECT_BUILD_ROOT,"release","out","c","*.o")
-  to = "#{ENVIRONMENT_PMESDR_TOP_DIR}/lib"
-  puts "from #{from}"
-  puts "to   #{to}"
-  FileUtils.cp(Dir.glob(from),to)
-
-  # I'm sure there's a better way to do this,
-  # (there has to be a way to access the list of file you get
-  # when you run rake files:header)
-  # but for now:
-  puts "Installing .h files:"
-  from = File.join(PROJECT_BUILD_ROOT,"..","src","*.h")
-  to = "#{ENVIRONMENT_PMESDR_TOP_DIR}/include"
-  puts "from #{from}"
-  puts "to   #{to}"
-  FileUtils.cp(Dir.glob(from),to)
+  # Need to figure out how to do cd src; make install
+  puts "Installing libcetb.a to PMESDR..."
+  Dir.chdir('src') do
+    sh "make install"
+  end
 
 end
 
