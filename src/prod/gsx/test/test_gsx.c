0552bbba282e4ccca1e28e41fe627fdab47231ea
Molly Hardman <mhardman@nsidc.org>
2015-09-01 13:05:17 -0600

    netcdf libs in project yaml - expanded tests
    Modified project.yaml to link netcdf libraries - note these are 4.1.3
    libs that come installed on the VM
    
    Added more tests to gsx_test - Ceedling module

--- src/prod/gsx/test/test_gsx.c
+++ src/prod/gsx/test/test_gsx.c
@@ -3,11 +3,11 @@
 #include "gsx.h"
 
 
-char file_name[FILENAME_MAX];
+char *file_name;
 
 void setUp(void)
 {
-  strcpy( file_name, "/home/vagrant/measures-byu/python/test_cetb_utilities_data/bgi_data/test_bgi.nc" );
+  file_name = strdup( "/home/vagrant/testing/GSX_SHORT.nc" );
 }
 
 void tearDown(void)
@@ -34,11 +34,47 @@ void test_gsx_valid_file ( void )
 {
   gsx_class *gsx;
 
-  //  gsx = gsx_init( file_name );
-  gsx = gsx_init( "/projects/PMESDR/vagrant/savoie/rss-2003/GSX_RSS_SSMI_FCDR_V07R00_F13_D20031231_S2309_E2343_R45267.nc" );
+  gsx = gsx_init( file_name );
   TEST_ASSERT_TRUE( gsx );
-  fprintf( stderr, "%s: netcdf file id %d \n", __FUNCTION__, *(gsx->fileid) );
-  //  gsx_close( gsx );
+  fprintf( stderr, "%s: netcdf file id %d \n", __FUNCTION__, gsx->fileid );
+  fprintf( stderr, "%s: netcdf dimensions %d\n", __FUNCTION__, gsx->dims );
+  gsx_close( gsx );
+}
+
+void test_gsx_literal_file ( void )
+{
+  gsx_class *gsx;
+
+  gsx = gsx_init( "/home/vagrant/measures-byu/python/test_cetb_utilities_data/bgi_data/test_bgi.nc" );
+  TEST_ASSERT_TRUE( gsx );
+  fprintf( stderr, "%s: netcdf file id %d \n", __FUNCTION__, gsx->fileid );
+  gsx_close( gsx );
+}
+
+void test_gsx_ndims ( void )
+{
+  gsx_class *gsx;
+  int status;
+
+  gsx = gsx_init ( file_name );
+  fprintf( stderr, "%s: netcdf file %s with id %d :\n\t: ndims=%d\n\t: nvars=%d\n\t: natts=%d\n\t: nunlim=%d\n",\
+	   __FUNCTION__, file_name, gsx->fileid, gsx->dims, gsx->vars, gsx->atts, gsx->unlimdims );
+  TEST_ASSERT_TRUE( 5 == gsx->dims );
+  gsx_close( gsx );
+}
+
+void test_gsx_dim_names ( void )
+{
+  gsx_class *gsx;
+  int status;
+
+  gsx = gsx_init ( file_name );
+  if ( NULL != gsx ){
+    status = gsx_inq_dims( gsx );
+  } else {
+    status = 1;
+  }
+  TEST_ASSERT_TRUE( 0 == status );
 }
 
   
