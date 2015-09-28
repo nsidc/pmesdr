9eff14cff4e0998b644dd0038e0e956997a64549
M. J. Brodzik <brodzik@nsidc.org>
2015-09-28 16:55:45 -0600

    Beginning add_sir_parameters functionality.
    Beginning functions to write SIR-specific file attributes.

--- src/prod/cetb_file/test/test_cetb_populate.c
+++ src/prod/cetb_file/test/test_cetb_populate.c
@@ -4,6 +4,7 @@
  * 01-Sep-2015 M. J. Brodzik brodzik@nsidc.org 303-492-8263
  * Copyright (C) 2015 Regents of the University of Colorado and Brigham Young University
  */
+#include <netcdf.h>
 #include <string.h>
 #include "unity.h"
 #include "cetb_file.h"
@@ -58,21 +59,53 @@ void setUp( void ) {
 
 void tearDown( void ) {
 
+}
+
+void test_cetb_populate_sir_parameters( void ) {
+
+  int nc_fileid=0;
+  int nits=20;
+  int median_filter=1;
+  int expected_nits=20;
+  int expected_median_filter=1;
+  
+  status = cetb_file_open( cetb );
+  TEST_ASSERT_TRUE_MESSAGE( 0 == status, "cetb_file_open" );
+  status = cetb_file_add_sir_parameters( cetb, nits, median_filter );
+  TEST_ASSERT_TRUE_MESSAGE( 0 == status, "cetb_file_add_sir_parameters" );
   cetb_file_close( cetb );
-}
 
-void test_cetb_open_with_bad_filename( void ) {
+  /* Confirm the expected values are in the output file */
+  status = nc_open( "./test/EASE2_N25km.F13_SSMI.1991001.19H.M.SIR.CSU.v0.1.nc",
+		    NC_NOWRITE, &nc_fileid );
+  TEST_ASSERT_TRUE( NC_NOERR == status );
 
-  cetb->filename = NULL;
-  status = cetb_file_open( cetb );
-  TEST_ASSERT_TRUE( 0 != status  );
+  status = nc_get_att_int( nc_fileid, NC_GLOBAL, "sir_number_of_iterations", &nits );
+  TEST_ASSERT_TRUE( NC_NOERR == status );
+  TEST_ASSERT_EQUAL_INT( expected_nits, nits );
+
+  status = nc_get_att_int( nc_fileid, NC_GLOBAL, "sir_median_filter", &median_filter );
+  TEST_ASSERT_TRUE( NC_NOERR == status );
+  TEST_ASSERT_EQUAL_INT( expected_median_filter, median_filter );
+  nc_close( nc_fileid );
   
 }
 
-void test_cetb_open( void ) {
+void test_cetb_populate_bgi_parameters_on_sir_file( void ) {
 
-  status = cetb_file_open( cetb );
-  TEST_ASSERT_TRUE( 0 == status );
+  int nc_fileid=0;
+  double gamma=0.0D;
+  double dimensionless=1.0D;
+  double db_threshold=2.0D;
+  double diff_threshold=3.0D;
   
+  status = cetb_file_open( cetb );
+  TEST_ASSERT_TRUE_MESSAGE( 0 == status, "cetb_file_open" );
+  status = cetb_file_add_bgi_parameters( cetb, gamma, dimensionless,
+					 db_threshold, diff_threshold );
+  TEST_ASSERT_TRUE_MESSAGE( 0 != status, "cetb_file_add_bgi_parameters" );
+  cetb_file_close( cetb );
+
 }
 
+    
