22ce9313611ccb533dc7b31ee407dce92a0e2cc3
M. J. Brodzik <brodzik@nsidc.org>
2015-11-11 17:35:29 -0700

    New utils modules.
    Created new utils module that currently just has one routine, for
    allocation of clean aligned memory.  This module can also be used to
    include general routines that we will use across multiple other modules.

---
+++ src/prod/utils/src/utils.h
@@ -0,0 +1,12 @@
+/*
+ * utils.h - General utilities for Passive Microwave ESDR project
+ *
+ * 11-Nov-2015 M. J. Brodzik brodzik@nsidc.org 303-492-8263
+ * Copyright (C) 2015 Regents of the University of Colorado and Brigham Young University
+ */
+#ifndef utils_H
+#define utils_H
+
+int utils_allocate_clean_aligned_memory( void **this, size_t size );
+
+#endif // utils_H
