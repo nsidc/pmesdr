f7453f3fd5ef6f56a4926762455e5b27996b214e
Paul Madden <maddenp@colorado.edu>
2016-10-13 08:48:18 -0600

    Mail unit-test output

--- janus_batchfiles/unit_test_remote.sh
+++ janus_batchfiles/unit_test_remote.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+#recipients=mhardman@nsidc.org brodzik@nsidc.org
+recipients=maddenp@colorado.edu
+
 source /etc/profile.d/modules.sh
 set -ex
 repo=$(readlink -f $(dirname $(readlink -f $0))/..)
 cd $repo
 git pull
 node=janus-compile$(shuf -i1-4 -n1)
-ssh $node $repo/janus_batchfiles/unit_test_local
+subject="measures-byu unit tests $(date)"
+ssh $node $repo/janus_batchfiles/unit_test_local.sh 2>&1 | mail -s "$subject" $recipients
