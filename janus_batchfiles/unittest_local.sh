1b5316c95360528d59972e9a6502da70db23de87
Paul Madden <maddenp@colorado.edu>
2016-10-13 07:51:40 -0600

    Parameterize ruby and ceedling versions

--- janus_batchfiles/unittest_local.sh
+++ janus_batchfiles/unittest_local.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+ceedling_version=0.18.0
+ruby_version=2.2.3
+
 # Set up the environment.
 
 source /etc/profile.d/modules.sh
 repo=$(readlink -f $(dirname $(readlink -f $0))/..)
 source $repo/src/prod/set_pmesdr_environment.sh -a -c gcc
-module load ruby/2.2.3
+module load ruby/$ruby_version
 set -ex
 export C_INCLUDE_PATH=$CURC_NETCDF_INC:$CURC_UDUNITS_INC:$repo/src/prod/utils/src:$repo/include
 export LIBRARY_PATH=$CURC_HDF5_LIB:$CURC_NETCDF_LIB:$CURC_UDUNITS_LIB
@@ -25,7 +28,7 @@ nosetests test_cetb_utilities.py
 # Ceedling tests.
 
 export GEM_HOME=$HOME/.gems
-gem install ceedling -v 0.18.0
+gem list | grep -q "ceedling ($ceedling_version)" || gem install ceedling -v $ceedling_version
 cd $repo/src/prod/cetb_file
 rake test:all
 cd $repo/src/prod/gsx
