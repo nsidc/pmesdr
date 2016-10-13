3847308a6d3376f5d0c998f28e6ed1ef00ac4560
Paul Madden <maddenp@colorado.edu>
2016-10-13 07:43:38 -0600

    Load ruby module before tracing starts

--- janus_batchfiles/unittest_local.sh
+++ janus_batchfiles/unittest_local.sh
@@ -4,6 +4,7 @@
 
 repo=$(readlink -f $(dirname $(readlink -f $0))/..)
 source $repo/src/prod/set_pmesdr_environment.sh -a -c gcc
+module load ruby/2.2.3
 set -ex
 export C_INCLUDE_PATH=$CURC_NETCDF_INC:$CURC_UDUNITS_INC:$repo/src/prod/utils/src:$repo/include
 export LIBRARY_PATH=$CURC_HDF5_LIB:$CURC_NETCDF_LIB:$CURC_UDUNITS_LIB
@@ -22,7 +23,6 @@ nosetests test_cetb_utilities.py
 
 # Ceedling tests.
 
-module load ruby/2.2.3
 export GEM_HOME=$HOME/.gems
 gem install ceedling -v 0.18.0
 cd $repo/src/prod/cetb_file
