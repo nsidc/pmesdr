15423a425257c86a7e2bfe1683b59a0a89157def
M. J. Brodzik <brodzik@nsidc.org>
2018-01-18 12:13:17 -0700

    Combining separate quick/daily scripts to run with arguments.

--- summit_batchfiles/build_pmesdr.sh
+++ summit_batchfiles/build_pmesdr.sh
@@ -2,6 +2,9 @@
 # Lines starting with #PBS are treated by bash as comments, but interpreted by qsub
 # as arguments.
 #
+# Re-builds a fresh copy of the system at PMESDR_TOP_DIR, and
+# run unit tests on the python utilities needed for regression testing.
+#
 # This script requires the name of a conda env to use to run the unit tests for CETB
 # comparison utilities.
 
--- summit_batchfiles/local_git_pull.sh
+++ summit_batchfiles/local_git_pull.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -l
 
-echo -e '\nBegin PMESDR system build...\n'
+echo -e '\nFetching latest software changes to PMESDR_regression...\n'
 hostname
-source /projects/$USER/PMESDR_quick_regression_test/src/prod/summit_set_pmesdr_environment.sh
+source /projects/$USER/PMESDR_regression/src/prod/summit_set_pmesdr_environment.sh
 cd $PMESDR_TOP_DIR
 git pull
+git status
 
