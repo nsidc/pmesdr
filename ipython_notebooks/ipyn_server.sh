c396e1462375ad63360c00f983fab59f7713b2ac
M. J. Brodzik <brodzik@nsidc.org>
2021-02-23 15:30:21 -0700

    New templates for NRT and L1C data sets.
    Merged jupyter notebooks from SMAP and 0630 to now work for any
    of 5 possible data sets. Upversioned all datasets so that new
    production runs will include the GRD time files bug fix (v1.5 for
    all non-SMAP data sets, and v1.1 for SMAP)

--- ipython_notebooks/ipyn_server.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-ipython notebook --script --no-browser --notebook-dir="~/measures-byu/ipython_notebooks/" --ip=0.0.0.0
---
+++ ipython_notebooks/jn_server.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+jupyter notebook --no-browser --notebook-dir="~/measures-byu/ipython_notebooks/" --ip=0.0.0.0
