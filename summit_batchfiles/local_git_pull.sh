d7af6d4747eec05b18ce548ac0bd4b3ebcdb92fa
M. J. Brodzik <brodzik@nsidc.org>
2022-04-26 11:30:53 -0600

    Added error_exit in case of git failure during regression.

--- summit_batchfiles/local_git_pull.sh
+++ summit_batchfiles/local_git_pull.sh
@@ -1,9 +1,23 @@
 #!/bin/bash -l
 
-echo -e '\nFetching latest software changes to PMESDR_regression...\n'
-hostname
+PROGNAME=$(basename $0)
+
+error_exit() {
+    # Use for fatal program error
+    # Argument:
+    #   optional string containing descriptive error message
+    #   if no error message, prints "Unknown Error"
+
+    echo "${PROGNAME}: ERROR: ${1:-"Unknown Error"}" 1>&2
+    exit -1
+}
+
+printf "%s: On %s, fetching latest software changes...\n" ${PROGNAME} $(hostname)
+
 source /projects/$USER/PMESDR_regression/src/prod/summit_set_pmesdr_environment.sh
-cd $PMESDR_TOP_DIR
-git pull
+cd $PMESDR_TOP_DIR || error_exit "$LINENO: Error changing to PMESDR_TOP_DIR."
+git pull || error_exit "$LINENO: Error on git pull."
 git status
+
+printf "%s: Done\n" ${PROGNAME}
 
