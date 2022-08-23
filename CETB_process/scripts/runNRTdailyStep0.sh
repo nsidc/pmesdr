ba100f7ed7a3738ff13d6a8a1046557255e06e6d
Molly Hardman <mhardman@nsidc.org>
2022-08-23 17:00:57 -0600

    Step0 now uses gnu_parallel and set for alpine

--- CETB_process/scripts/runNRTdailyStep0.sh
+++ CETB_process/scripts/runNRTdailyStep0.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 #
-# This script must be preceded by running summit_set_pmesdr_environment.sh
-#   which is located in the {measures-byu-repo-dir}/src/prod
-#
 # script to run NRT daily Step0:
 #   fetch latest data - input argument l1c or ICDR or SMAP
 #   run gsx on input files
@@ -12,11 +9,14 @@
 #
 #SBATCH --qos normal
 #SBATCH --job-name runNRTdailyStep0
-#SBATCH --account=ucb135_summit3
+#SBATCH --account=ucb-general
+#SBATCH --partition=amilan
+#SBATCH --constraint=ib
 #SBATCH --time=01:00:00
+#SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --ntasks=20
-#SBATCH -o /scratch/summit/%u/NRTdaily_output/runNRTdailyStep0-%j.out
+#SBATCH -o /scratch/alpine/%u/NRTdaily_output/runNRTdailyStep0-%j.out
 # Set the system up to notify upon completion
 #SBATCH --mail-type=FAIL,REQUEUE,STAGE_OUT
 #SBATCH --mail-user=mhardman@nsidc.org
@@ -28,15 +28,13 @@ usage() {
     echo "Usage: `basename $0` [-trf] [-h] GSX_TYPE CONDAENV" 1>&2
     echo "  GSX_TYPE CONDAENV" 1>&2
     echo "Options: "  1>&2
-    echo "  -t: top level data location under /scratch/summit/${USER}" 1>&2
+    echo "  -t: top level data location under /scratch/alpine/${USER}" 1>&2
     echo "  -r: set base resolution default is 25km r -1 is 36km r -2 is 24km" 1>&2
     echo "  -h: display help message and exit" 1>&2
     echo "  -f: do the ftp pull(default is no ftp pull)" 1>&2
     echo "  GSX_TYPE : type of gsx translation to do (see gsx --help)" 1>&2
     echo "           : also controls which platforms to process, eg F17 or AQUA etc" 1>&2
     echo "  CONDAENV : conda environment for gsx translation" 1>&2
-    echo "Prior to running this script, do:" 1>&2
-    echo " run summit_set_pmesdr_environment.sh " 1>&2
     echo "" 1>&2
 }
 
@@ -88,16 +86,13 @@ while getopts "fr:t:h" opt; do
 	esac
 done
 
-if [[ -z ${PMESDR_TOP_DIR} ]];
-then
-    error_exit "$LINENO: First set the PMESDR environment run summit_set_pmesdr_environment.sh"
-fi
+source /projects/${USER}/measures-byu-alpine/src/prod/alpine_set_pmesdr_environment.sh
     
 date
 # Now set up sensor based on GSX type
 
 echo "$PROGNAME: top_level = $top_level"
-direc=/scratch/summit/${USER}/${top_level}
+direc=/scratch/alpine/${USER}/${top_level}
 echo "$PROGNAME: scratch directory $direc"
 
 shift $(($OPTIND - 1))
@@ -106,18 +101,17 @@ shift $(($OPTIND - 1))
 
 gsx_type=$1
 condaenv=$2
-source activate $condaenv
+
 # start sbatch for the next day
 sbatch --begin=04:30:00 --account=$SLURM_JOB_ACCOUNT ${PMESDR_RUN}/runNRTdailyStep0.sh ${ftp_string} ${res_string} ${arg_string} ${gsx_type} ${condaenv}
 ml purge
-ml intel
-ml impi
-ml loadbalance
-ml python/3.6.5
+ml intel/2022.1.2
+ml python/3
+ml gnu_parallel
 ml
 date
 
-thisHost=$(hostname)
+thisHost=$SLURM_NODELIST
 thisDate=$(date)
 echo "$PROGNAME: Begin on hostname=$thisHost on $thisDate"
 echo "$PROGNAME: SLURM_SCRATCH=$SLURM_SCRATCH"
@@ -128,14 +122,14 @@ echo "$PROGNAME: with gsx_type=$gsx_type and condaenv=$condaenv base_resolution=
 case $gsx_type in
     
     SSMIS-L1C)
-    fetch_file="/projects/${USER}/swathfetcher/ftp_nrt_l1c.py"
+    fetch_file="/projects/${USER}/swathfetcher/ftp_nrt_l1c_alpine.py"
     suffix="*.RT-H5"
     run_dir="/projects/${USER}/swathfetcher"
     make_file="all_SSMIS_make_for_sensor.sh"
     platforms="F16 F17 F18";;
     
     AMSR2-L1C)
-    fetch_file="/projects/${USER}/swathfetcher/ftp_nrt_amsr2_l1c.py"
+    fetch_file="/projects/${USER}/swathfetcher/ftp_nrt_amsr2_l1c_alpine.py"
     suffix="*.RT-H5"
     run_dir="/projects/${USER}/swathfetcher"
     make_file="all_SSMIS_make_for_sensor.sh"
@@ -163,6 +157,7 @@ echo "$PROGNAME: $platforms"
 
 # if -f is set download files from ftp
 if [[ $do_ftp ]]; then
+    source activate base
 #Go here so that correct secret files are used or SMAP downloaded to correct location
     cd ${run_dir}
     python ${fetch_file} || error_exit "Line $LINENO: ftp error."
@@ -172,23 +167,23 @@ if [[ $do_ftp ]]; then
 
 #after files are retrieved, create input file list for gsx of files with
 # modification date less than 1 day old
-
+    source activate $condaenv
     date
     for src in $platforms
     do
 	echo "working this src ${src}"
-	if [[ -f ${direc}/${src}_scripts/gsx_lb_list_summit ]]; then
-	    rm ${direc}/${src}_scripts/gsx_lb_list_summit
+	if [[ -f ${direc}/${src}_scripts/gsx_lb_list_alpine ]]; then
+	    rm ${direc}/${src}_scripts/gsx_lb_list_alpine
 	    echo "removed old gsx_lb_file for ${src}"
 	fi
 	for file in `find ${direc}/${src} -name "${suffix}" -mtime 0`
 	do
 	    basen=`basename ${file}`
 	    echo "gsx ${gsx_type} ${file} ${direc}/${src}_GSX/GSX_${basen}.nc" \
-		 >> ${direc}/${src}_scripts/gsx_lb_list_summit
+		 >> ${direc}/${src}_scripts/gsx_lb_list_alpine
 	done
-	echo "mpirun -genv I_MPI_FABRICS=shm:ofi lb ${direc}/${src}_scripts/gsx_lb_list_summit"
-	mpirun -genv I_MPI_FABRICS=shm:ofi lb ${direc}/${src}_scripts/gsx_lb_list_summit || \
+	echo "parallel -a ${direc}/${src}_scripts/gsx_lb_list_alpine"
+	parallel -a ${direc}/${src}_scripts/gsx_lb_list_alpine || \
 	    error_exit "Line $LINENO: mpirun gsx ${src} error."
     done
 fi
@@ -200,11 +195,13 @@ startyear=`date '+%Y' -d "7 days ago"`
 startdoy=`date '+%j' -d "7 days ago"`
 endyear=`date '+%Y'`
 enddoy=`date '+%j'`
+echo "$PROGNAME: $startyear=start year $startdoy=start doy $endyear=end year $enddoy=end doy"
+echo "$PROGRAME: $platforms=platforms $src=src"
 
 # Note the CSU ICDR data stream is 1-2 days behind today
 # This workaround gets around a bug in meas_meta_setup that fails if there
 # are no input files in the make file
-if [[ ${gsx_type} -eq SSMIS-CSU-ICDR ]]; then
+if [[ ${gsx_type} == SSMIS-CSU-ICDR ]]; then
     endyear=`date '+%Y' -d "2 days ago"`
     enddoy=`date '+%j' -d "2 days ago"`
 fi
@@ -235,14 +232,11 @@ do
     out=$(${PMESDR_RUN}/${make_file} ${res_string} $startyear $startdoy $endyear \
 	   $enddoy $src ${PMESDR_SCRIPT_DIR} $top_level) || \
 	error_exit "Line $LINENO: all_SSMIS(or SMAP)_make_for_sensor ${src} error."
-    ml intel
-    ml netcdf/4.4.1.1
+    ml netcdf/4.8.1
     ml udunits/2.2.25
-    ml impi
-    ml loadbalance
-    echo "mpirun lb ${direc}/${src}_scripts/${src}_make_list${suffix}"
-    mpirun -genv I_MPI_FABRICS=shm:ofi lb ${direc}/${src}_scripts/${src}_make_list${suffix} || \
-	error_exit "Line $LINENO: mpirun meas_meta_make ${src} error."
+    echo "parallel -a ${direc}/${src}_scripts/${src}_make_list${suffix}"
+    parallel -a ${direc}/${src}_scripts/${src}_make_list${suffix} || \
+	error_exit "Line $LINENO: parallel meas_meta_make ${src} error."
 done
 
 #finally set off Step1
