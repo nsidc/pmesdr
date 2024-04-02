8737805ac14e798aeef9daca3d4490b6c637dc66
Molly Hardman <mhardman@nsidc.org>
2024-04-02 16:28:30 -0600

    Recreating files for scrontab remote build and regression tests

--- alpine_batchfiles/local_git_pull.sh
+++ alpine_batchfiles/local_git_pull.sh
@@ -14,7 +14,7 @@ error_exit() {
 
 printf "%s: On %s, fetching latest software changes...\n" ${PROGNAME} $(hostname)
 
-source /projects/$USER/PMESDR_regression/src/prod/summit_set_pmesdr_environment.sh
+source /projects/$USER/PMESDR_regression/src/prod/single_set_pmesdr_environment.sh
 cd $PMESDR_TOP_DIR || error_exit "$LINENO: Error changing to PMESDR_TOP_DIR."
 git pull || error_exit "$LINENO: Error on git pull."
 git status
