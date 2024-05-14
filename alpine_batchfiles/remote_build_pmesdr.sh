95e5b11c80723ee5bc3993f9f2b5f82858755307
Molly Hardman <mhardman@nsidc.org>
2024-05-14 16:21:04 -0600

    replaced acompoile with salloc command in remote_build

--- alpine_batchfiles/remote_build_pmesdr.sh
+++ alpine_batchfiles/remote_build_pmesdr.sh
@@ -4,7 +4,7 @@
 
 condaenv=$1
 
-acompile
+salloc --nodes=1 --partition=acompile --ntasks=1  --time=01:00:00  --qos=compile --job-name=acompile --bell --oversubscribe srun --pty /bin/bash
 ssh -n -o StrictHostKeyChecking=no $SLURM_NODELIST "set -e ; echo -e '\nBegin PMESDR system build...\n'; source ${PMESDR_TOP_DIR}/src/prod/single_set_pmesdr_environment.sh ; source ${PMESDR_TOP_DIR}/alpine_batchfiles/build_pmesdr.sh ${condaenv} ; echo -e '\nEnd PMESDR system build.\n' "
 # End of compile job shell script
 #
