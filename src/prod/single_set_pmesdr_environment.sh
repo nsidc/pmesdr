d6d81f6139c7228733edee9df1f20cd74725bae5
M. J. Brodzik <brodzik@nsidc.org>
2024-07-09 15:06:01 -0600

    Simplifying logic for various environments.
    Trying to guess at gcc compiler in default environment.

--- src/prod/single_set_pmesdr_environment.sh
+++ src/prod/single_set_pmesdr_environment.sh
@@ -23,11 +23,6 @@
 
 # Make no assumptions about starting LOCALE
 export LOCALE=
-#
-# First check for compiler environment variable - cmd line args will override
-#
-compiler=icc
-export PMESDR_COMPILER=$compiler
 
 # Grab the full path to this script, regardless of where it is called from.
 export PMESDR_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -35,60 +30,60 @@ export PMESDR_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # The Project's Top Level Directory is two directories up from this file:
 export PMESDR_TOP_DIR=$(dirname $(dirname "$PMESDR_SCRIPT_DIR"))
 echo "PMESDR_TOP_DIR is" $PMESDR_TOP_DIR
+
 # PMESDR environment variables for running the system
 export meas_home=$PMESDR_TOP_DIR
 
+# Regression data is stored by the date it was created/modified
 regression_yyyymmdd=20230125
 export PMESDR_REGRESS_DIR=$PMESDR_TOP_DIR/../pmesdr_regression_data/${regression_yyyymmdd}
+export PMESDR_TESTDATA_DIR=$PMESDR_TOP_DIR/sample_data/test_gsx
+export PMESDR_CONDAENV=pmesdrEnv
+export PMESDR_RUN=${PMESDR_TOP_DIR}/CETB_process/scripts
 
 # Determine the LOCALE and set compiler and more environment variables
-thisHostname=`hostname --fqdn`
+thisHostname=`hostname -f`
 
+echo "Setting environment..."
 if [[ "$thisHostname"  == *"int.nsidc.org"* ]]; then
 
-  compiler=gcc
-  export PMESDR_COMPILER=$compiler
+  export PMESDR_COMPILER=gcc
   export LOCALE=int.nsidc.org
   export PATH=/opt/anaconda/bin:$PATH
   export PMESDR_COMPARE_TOLERANCE=0.01
   export PMESDR_MAX_DIFF_PIXELS=100
-  export PMESDR_REGRESS_DIR=/projects/PMESDR/pmesdr_regression_data/${regression_yyyymmdd}
-  export PMESDR_TESTDATA_DIR=$PMESDR_TOP_DIR/sample_data/test_gsx
-  export PMESDR_CONDAENV=pmesdrEnv
 
-fi
-
-if [[ "$thisHostname" == *"rc.colorado.edu" \
+elif [[ "$thisHostname" == *"rc.colorado.edu" \
 	  || "$thisHostname" == *"c3"* ]]; then
 
-  echo "Setting environment..."
-  export PMESDR_RUN=${PMESDR_TOP_DIR}/CETB_process/scripts
+  export PMESDR_COMPILER=icc
+  export LOCALE=ALPINEicc
   export PMESDR_COMPARE_TOLERANCE=0.06
   export PMESDR_MAX_DIFF_PIXELS=100
-  export PMESDR_REGRESS_DIR=$PMESDR_TOP_DIR/../pmesdr_regression_data/${regression_yyyymmdd}
-  export PMESDR_CONDAENV=pmesdrEnv
 
-  echo "Setting netcdf for the icc compiler on Alpine"
   module purge all
   ml intel/2022.1.2
   ml netcdf/4.8.1
   ml udunits/2.2.25
   ml git-lfs/3.1.2
-  export LOCALE=ALPINEicc
   module list
-fi
 
-if [[ "$thisHostname" == "pmesdr-nrt" ]]; then
-  echo "Setting netcdf for the icc compiler on Cumulus"
-  source /opt/intel/oneapi/setvars.sh
+elif [[ "$thisHostname" == "pmesdr-nrt" ]]; then
+
+  # this environment is assuming CURC Cumulus
+  export PMESDR_COMPILER=icc
   export LOCALE=CUMULUSicc
-
-  echo "Setting environment..."
-  export PMESDR_RUN=${PMESDR_TOP_DIR}/CETB_process/scripts
+  source /opt/intel/oneapi/setvars.sh
   export PMESDR_COMPARE_TOLERANCE=0.06
   export PMESDR_MAX_DIFF_PIXELS=100
-  export PMESDR_REGRESS_DIR=${PMESDR_TOP_DIR}/../pmesdr_regression_data/${regression_yyyymmdd}
-  export PMESDR_CONDAENV=pmesdrEnv
+
+else
+
+  export PMESDR_COMPILER=gcc
+  export LOCALE=default
+  export PMESDR_COMPARE_TOLERANCE=0.06
+  export PMESDR_MAX_DIFF_PIXELS=100
+  
 fi                                                                                                                   
 
 if [[ -z $LOCALE ]]; then
@@ -96,6 +91,8 @@ if [[ -z $LOCALE ]]; then
 else
   echo "Hostname: $thisHostname"
   echo "Compiler: $PMESDR_COMPILER"
+  echo "Conda env: $PMESDR_CONDAENV"
+  echo "Regression data: $PMESDR_REGRESS_DIR"
   echo "It is expected that you will use conda environments for specific versions of python"
   echo "PMESDR system LOCALE=$LOCALE, ready to use the PMESDR system."
 fi
